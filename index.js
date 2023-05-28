e850db5b470ab2eb73f44b5b2a577be064a822f9
jumailj <jumailj.work@gmail.com>
2023-05-28 10:02:43 +0530

    added json objects

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const fs= require('fs');
 const http = require('http');
 
-const overviewpage= fs.readFileSync(`${__dirname}/template/template-overview.html`, 'utf-8');
 
-console.log(overviewpage);
+let overviewpage= fs.readFileSync(`${__dirname}/template/template-overview.html`, 'utf-8');
+let productCard = fs.readFileSync(`${__dirname}/template/template-card.html`, 'utf-8');
+let fruits = JSON.parse( fs.readFileSync(`${__dirname}/data/fruit-data.json`));
+let prodcutCards = '';
+console.log(fruits);
 
+
+productCard = productCard.replace(/<%FRUIT_EMOJI%>/mg, fruits[0].emoji); 
+productCard = productCard.replace(`<%FRUIT_NAME%>`, fruits[0].fruitName);
+productCard = productCard.replace(`<%NO_OF_FRUIT%>`, fruits[0].noOfFruit);
+productCard = productCard.replace(`<%RATE_OF_FRUIT%>`, fruits[0].rateOfFruit);
+
+prodcutCards+= productCard;
+
+
+//console.log(productCard);
+overviewpage = overviewpage.replace(`<%PRODUCTS%>`, prodcutCards)
 
 const server= http.createServer((req,res)=>{
 
     res.end(overviewpage);
 });
-
 
 
 server.listen(8000,'127.0.0.1',()=>{
